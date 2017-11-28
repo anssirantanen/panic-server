fd65ae3749119808a59af33bd395d5e0d9a9411f
anssirantanen <aparantanen@gmail.com>
2017-11-28 20:56:47 +0200

    basic db schema.

--- src/main/scala/db/Connector.scala
+++ src/main/scala/db/Connector.scala
@@ -6,7 +6,7 @@ import com.outworkers.phantom.dsl._
 
 
 object Connector {
- /* val connector: CassandraConnection = ContactPoint.local
+  val connector: CassandraConnection = ContactPoint.local
     .withClusterBuilder(_.withSocketOptions(
       new SocketOptions()
         .setConnectTimeoutMillis(20000)
@@ -17,6 +17,6 @@ object Connector {
     KeySpace("panic").ifNotExists().`with`(
       replication eqs SimpleStrategy.replication_factor(1)
     )
-  )*/
-  val connector : CassandraConnection = ContactPoint("localhost",9042).keySpace("panic")
+  )
+  //val connector : CassandraConnection = ContactPoint("localhost",9042).keySpace("panic")
 }
---
+++ src/main/scala/db/FrameTable.scala
@@ -0,0 +1,19 @@
+package db
+
+import com.outworkers.phantom.Table
+import com.outworkers.phantom.jdk8.ZonedDateTime
+import com.outworkers.phantom.keys.{PartitionKey, PrimaryKey}
+import com.outworkers.phantom.jdk8.indexed._
+
+
+//table mapping Table[cassandratable , case class]
+abstract class FrameTable extends Table[FrameTable, models.Frame]{
+  object identity extends StringColumn with PartitionKey
+  object tag extends OptionalStringColumn
+  object level extends IntColumn
+  object time extends Col[ZonedDateTime] with PrimaryKey
+  object message extends StringColumn with PrimaryKey
+
+}
+
+
