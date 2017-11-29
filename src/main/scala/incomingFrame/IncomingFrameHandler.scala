d96710405c9a58cc58903a77b835a91ceee33590
anssirantanen <aparantanen@gmail.com>
2017-11-29 23:07:55 +0200

    Basic cassandra phantom connection without successful insert.

--- src/main/scala/incomingFrame/IncomingFrameHandler.scala
+++ src/main/scala/incomingFrame/IncomingFrameHandler.scala
@@ -2,23 +2,25 @@ package incomingFrame
 
 import incomingFrame.IncomingFrameHandler.TextMessage
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import db.DatabaseInsertActor
 import models.Frame
 import uiComponents.WebsocketListeners
 
 
 object IncomingFrameHandler{
   case class TextMessage(textMessage: String)
-  def props() : Props = Props(new IncomingFrameHandler)
+  def props : Props = Props(new IncomingFrameHandler)
 }
 
 class IncomingFrameHandler extends Actor with ActorLogging{
-  val websocketListeners: ActorRef = context.actorOf(WebsocketListeners.props(), "monitor-websockets-actor")
-
+  val websocketListeners: ActorRef = context.actorOf(WebsocketListeners.props, "monitor-websockets-actor")
+  val databaseInsertActor: ActorRef = context.actorOf(DatabaseInsertActor.props)
   log.error(self.path.toString)
   override def receive: Receive = {
     case message : TextMessage =>
       websocketListeners ! message
     case endPointMessage : Frame =>
+      databaseInsertActor ! Frame
       websocketListeners ! endPointMessage
   }
 }
